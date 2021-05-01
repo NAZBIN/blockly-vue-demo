6afb9b3dd3841b18eb3d9559f5a7874da421c5bf
qiuwenbin <qiuwenbin@xiaomi.com>
2021-05-01 21:56:02 +0800

    add: demo end

---
+++ src/App.vue
@@ -0,0 +1,117 @@
+<template>
+  <div id="app">
+    <BlocklyComponent id="blockly" :options="options" ref="foo"></BlocklyComponent>
+    <p id="code">
+      <button v-on:click="showCode()">Show JavaScript</button>
+      <pre v-html="code" id="codeBlock"></pre>
+    </p>
+  </div>
+</template>
+
+<script>
+import BlocklyComponent from './components/BlocklyComponent.vue'
+import './blocks/stocks';
+import './prompt';
+import BlocklyJS from 'blockly/javascript';
+
+export default {
+  name: 'app',
+  components: {
+    BlocklyComponent
+  },
+  data(){
+    return {
+      code: '',
+      options: {
+        media: 'media/',
+        grid:
+          {
+            spacing: 25,
+            length: 3,
+            colour: '#ccc',
+            snap: true
+          },
+        toolbox:
+        `<xml>
+          <category name="Logic" colour="%{BKY_LOGIC_HUE}">
+            <block type="controls_if"></block>
+            <block type="logic_compare"></block>
+            <block type="logic_operation"></block>
+            <block type="logic_negate"></block>
+            <block type="logic_boolean"></block>
+          </category>
+          <category name="Loops" colour="%{BKY_LOOPS_HUE}">
+            <block type="controls_repeat_ext">
+              <value name="TIMES">
+                <block type="math_number">
+                  <field name="NUM">10</field>
+                </block>
+              </value>
+            </block>
+            <block type="controls_whileUntil"></block>
+          </category>
+          <category name="Math" colour="%{BKY_MATH_HUE}">
+            <block type="math_number">
+              <field name="NUM">123</field>
+            </block>
+            <block type="math_arithmetic"></block>
+            <block type="math_single"></block>
+          </category>
+          <category name="Text" colour="%{BKY_TEXTS_HUE}">
+            <block type="text"></block>
+            <block type="text_length"></block>
+            <block type="text_print"></block>
+          </category>
+          <category name="Variables" custom="VARIABLE" colour="%{BKY_VARIABLES_HUE}">
+            </category>
+          <category name="Stocks" colour="%{BKY_LOOPS_HUE}">
+            <block type="stock_buy_simple"></block>
+            <block type="stock_buy_prog"></block>
+            <block type="stock_fetch_price"></block>
+          </category>
+        </xml>`
+      }
+    }
+  },
+  methods: {
+    showCode() {
+      let codeBlock = document.querySelector("#codeBlock");
+      codeBlock.style.display = "block";
+      this.code = BlocklyJS.workspaceToCode(this.$refs["foo"].workspace);
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+html, body {
+  margin: 0;
+}
+
+#code {
+  position: absolute;
+  right: 0;
+  top: 0;
+  width: 30%;
+  height: 100%;
+  margin: 0;
+  background-color: beige;
+  border-radius: 10px;
+}
+
+
+#blockly {
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 70%;
+  height: 100%;
+}
+</style>
